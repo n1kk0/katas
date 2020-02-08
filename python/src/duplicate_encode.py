981b40907d0563068f40c258a3a9851fca6a31ad
n1kk0 <nicolas.moussikian@gmail.com>
2020-02-08 16:32:49 +0100

    duplicate encore

---
+++ python/src/duplicate_encode.py
@@ -0,0 +1,17 @@
+def duplicate_encode(word):
+    letters = {}
+    out = ""
+
+    for letter in word:
+        if letters.get(letter):
+            letters[letter.lower()] += 1
+        else:
+            letters[letter.lower()] = 1
+
+    for letter in word:
+        if letters[letter.lower()] > 1:
+            out += ")"
+        else:
+            out += "("
+
+    return out
---
+++ python/test/test_duplicate_encode.py
@@ -0,0 +1,15 @@
+import unittest
+
+from src.duplicate_encode import duplicate_encode
+
+
+class TestDuplicateEncode(unittest.TestCase):
+    def test_duplicate_encode(self):
+        self.assertEqual(duplicate_encode("din"), "(((")
+        self.assertEqual(duplicate_encode("recede"), "()()()")
+        self.assertEqual(duplicate_encode("Success"), ")())())", "should ignore case")
+        self.assertEqual(duplicate_encode("(( @"), "))((")
+
+
+if __name__ == '__main__':
+    unittest.main()

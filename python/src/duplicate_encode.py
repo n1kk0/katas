c45ef3de92ff9db618f0f9e06d4a0462d3bf1cad
n1kk0 <nicolas.moussikian@gmail.com>
2020-02-08 17:18:05 +0100

    best answer

--- python/src/duplicate_encode.py
+++ python/src/duplicate_encode.py
@@ -1,4 +1,13 @@
 def duplicate_encode(word):
+    '''
+    best:
+    return "".join([
+        "("
+        if word.lower().count(letter) == 1
+        else ")"
+        for letter in word.lower()
+    ])
+    '''
     letters = {}
     out = ""
 

47045ed2dc13757d33271b737cb38f2b0d401d74
n1kk0 <nicolas.moussikian@gmail.com>
2020-02-08 16:35:14 +0100

    lint

--- python/test/test_duplicate_encode.py
+++ python/test/test_duplicate_encode.py
@@ -7,7 +7,13 @@ class TestDuplicateEncode(unittest.TestCase):
     def test_duplicate_encode(self):
         self.assertEqual(duplicate_encode("din"), "(((")
         self.assertEqual(duplicate_encode("recede"), "()()()")
-        self.assertEqual(duplicate_encode("Success"), ")())())", "should ignore case")
+
+        self.assertEqual(
+            duplicate_encode("Success"),
+            ")())())",
+            "should ignore case"
+        )
+
         self.assertEqual(duplicate_encode("(( @"), "))((")
 
 

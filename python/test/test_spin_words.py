efa6a9443f0ef7ab1bdabaebaaad185852b58ed4
n1kk0 <nicolas.moussikian@gmail.com>
2020-02-15 12:19:19 +0100

    lint

--- python/test/test_spin_words.py
+++ python/test/test_spin_words.py
@@ -6,7 +6,11 @@ from src.spin_words import spin_words
 class TestRenderTree(unittest.TestCase):
     def test_spin_words(self):
         self.assertEqual(spin_words("Welcome"), "emocleW")
-        self.assertEqual(spin_words("Bonjour bonjour les petits enfants"), "ruojnoB ruojnob les stitep stnafne")
+
+        self.assertEqual(
+            spin_words("Bonjour bonjour les petits enfants"),
+            "ruojnoB ruojnob les stitep stnafne"
+        )
 
 
 if __name__ == '__main__':

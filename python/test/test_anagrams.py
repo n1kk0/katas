c080cf6077a0b071b91c70635749ce8f1fa4a7e6
n1kk0 <nicolas.moussikian@gmail.com>
2020-02-25 10:03:59 +0100

    lint

--- python/test/test_anagrams.py
+++ python/test/test_anagrams.py
@@ -5,8 +5,21 @@ from src.anagrams import anagrams
 
 class TestDuplicateEncode(unittest.TestCase):
     def test_anagrams(self):
-        self.assertEqual(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa'])
-        self.assertEqual(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer'])
+        self.assertEqual(
+            anagrams(
+                'abba',
+                ['aabb', 'abcd', 'bbaa', 'dada']
+            ),
+            ['aabb', 'bbaa']
+        )
+
+        self.assertEqual(
+            anagrams(
+                'racer',
+                ['crazer', 'carer', 'racar', 'caers', 'racer']
+            ),
+            ['carer', 'racer']
+        )
 
 
 if __name__ == '__main__':

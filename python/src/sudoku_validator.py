848cda679d5de2a2b1d883029a62a56cdd7a082c
n1kk0 <nicolas.moussikian@gmail.com>
2020-02-26 17:25:18 +0100

    sudoku validator

---
+++ python/src/sudoku_validator.py
@@ -0,0 +1,24 @@
+def valid_solution(board):
+    for i in list(range(1, 10)):
+        if ''.join([str(x) for x in sorted(board[i - 1])]) != '123456789':
+            return False
+
+        if ''.join([
+            str(x) for x in sorted([
+                board[y - 1][i - 1] for y in range(1, 10)
+            ])
+        ]) != '123456789':
+            return False
+
+        if i % 3 == 1:
+            for j in list(range(3)):
+                if ''.join([
+                    str(x) for x in sorted([
+                        board[k][l]
+                            for l in list(range(j * 3, (j * 3) + 3))
+                            for k in list(range(i - 1, i + 2))
+                    ])
+                ]) != '123456789':
+                    return False
+
+    return True

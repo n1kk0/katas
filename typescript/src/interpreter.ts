f30008b5b262bef24e972062f79dfc858675d187
n1kk0 <nicolas.moussikian@gmail.com>
2020-03-02 09:00:19 +0100

    maintainability

--- typescript/src/interpreter.ts
+++ typescript/src/interpreter.ts
@@ -4,49 +4,57 @@ interface BracketRelation {
 }
 
 export function interpreter(code: string, tape: string): string {
-    var tapePointer = 0;
-  
     const bracketsRelations = parseBrackets(code);
+    let tapePointer = 0;
 
-    for(var i = 0; i < code.length; i++) {
-        switch (code[i]) {
-            case '*':
-                tape = tape.substr(0, tapePointer) +
-                    (tape[tapePointer] == "0" ? "1" : "0") +
-                    tape.substr(tapePointer + 1)
-                ;
+    for(let codeStep = 0; codeStep < code.length; codeStep++) {
+        if (code[codeStep] == '*') {
+            tape = tape.substr(0, tapePointer) +
+                (tape[tapePointer] == "0" ? "1" : "0") +
+                tape.substr(tapePointer + 1)
+            ;
+        }
 
-                break;
-            case '>':
-                tapePointer++;
-                break;
-            case '<':
-                tapePointer--;
-                break;
-            case '[':
-                if (tape[tapePointer] == "0") {
-                    let bracket = bracketsRelations.find((v) => v.open == i);
+        if (code[codeStep] == '>') {
+            tapePointer++;
+        }
 
-                    i = bracket == undefined ? 0 : bracket.close;
-                }
+        if (code[codeStep] == '<') {
+            tapePointer--;
+        }
 
-                break;
-            case ']':
-                if (tape[tapePointer] == "1") {
-                    let bracket = bracketsRelations.find((v) => v.close == i);
+        if (code[codeStep] == '[' && tape[tapePointer] == "0") {
+            codeStep = getClosingBrace(bracketsRelations, codeStep);
+        }
 
-                    i = bracket == undefined ? 0 : bracket.open;
-                }
-
-                break;
+        if (code[codeStep] == ']' && tape[tapePointer] == "1") {
+            codeStep = getOpeningBrace(bracketsRelations, codeStep);
         }
 
         if (tapePointer < 0 || tapePointer >= tape.length) {
-            i = code.length;
+            codeStep = code.length;
         }
     }
 
     return tape;
+}
+
+function getOpeningBrace(
+    bracketsRelations: BracketRelation[],
+    codeStep: number
+) {
+    let bracket = bracketsRelations.find((v) => v.close == codeStep);
+
+    return bracket == undefined ? 0 : bracket.open;
+}
+
+function getClosingBrace(
+    bracketsRelations: BracketRelation[],
+    codeStep: number
+) {
+    let bracket = bracketsRelations.find((v) => v.open == codeStep);
+
+    return bracket == undefined ? 0 : bracket.close;
 }
 
 function parseBrackets(code: string): BracketRelation[] {

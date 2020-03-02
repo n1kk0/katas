b92d234e1ff70a9e39dd4114a3834e44c3b9a40f
n1kk0 <nicolas.moussikian@gmail.com>
2020-03-02 01:03:21 +0100

    maintanability test

--- typescript/src/is_interesting.ts
+++ typescript/src/is_interesting.ts
@@ -2,45 +2,7 @@ export function isInteresting(n: number, awesomePhrases: number[]): number {
   var returnValue = 0;
 
   for (var i = 2; i >= 0; i--) {
-    if ((n + i).toString().slice(-2) === '00') {
-      returnValue = i > 0 ? 1 : 2;
-    }
-
-    if (
-      n + i > 99 &&
-      (n + i).toString().split('').every(
-        (value, _, values) => value === values[0]
-      )
-    ) {
-      returnValue = i > 0 ? 1 : 2;
-    }
-
-    if (
-      n + i > 99 &&
-      (n + i).toString().split('').every(
-        (value, index, array) =>
-          index === (n + i).toString().length - 1 ||
-          ((+value + 1) % 10).toString() === array[index + 1]
-      )
-    ) {
-      returnValue = i > 0 ? 1 : 2;
-    }
-
-    if (
-      n + i > 99 &&
-      (n + i).toString().split('').every(
-        (value, index, array) =>
-          index === 0 ||
-          (+value + 1).toString() === array[index - 1]
-      )
-    ) {
-      returnValue = i > 0 ? 1 : 2;
-    }
-
-    if (
-      n + i > 99 &&
-      (n + i).toString().split('').reverse().join('') === (n + i).toString()
-    ) {
+    if (testItem(n + i)) {
       returnValue = i > 0 ? 1 : 2;
     }
 
@@ -50,4 +12,45 @@ export function isInteresting(n: number, awesomePhrases: number[]): number {
   }
 
   return returnValue;
+}
+
+function testItem(n: number) {
+  return isMultipleOf100(n) ||
+    hasSameItems(n) ||
+    hasAscendingItems(n) ||
+    hasDescendingItems(n) ||
+    hasPalidrome(n)
+  ;
+}
+
+function isMultipleOf100(n: number) {
+  return n.toString().slice(-2) === '00';
+}
+
+function hasSameItems(n: number) {
+  return n > 99 && n.toString().split('').every(
+    (value, _, values) => value === values[0]
+  );
+}
+
+function hasAscendingItems(n: number) {
+  return n > 99 && n.toString().split('').every(
+    (value, index, values) =>
+      index === n.toString().length - 1 ||
+      ((+value + 1) % 10).toString() === values[index + 1]
+  );
+}
+
+function hasDescendingItems(n: number) {
+  return n > 99 && n.toString().split('').every(
+    (value, index, values) =>
+      index === 0 ||
+      (+value + 1).toString() === values[index - 1]
+  );
+}
+
+function hasPalidrome(n: number) {
+  return n > 99 &&
+    n.toString().split('').reverse().join('') === n.toString()
+  ;
 }

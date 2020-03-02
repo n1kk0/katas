c2577bf4e16fd8f35a4ba3d1799d9970c8c1a8f7
n1kk0 <nicolas.moussikian@gmail.com>
2020-03-02 09:40:04 +0100

    maintainability

--- python/src/sudoku_validator.py
+++ python/src/sudoku_validator.py
@@ -1,3 +1,16 @@
+def check_sub_squares(board, i):
+    for j in list(range(3)):
+        if ''.join([
+            str(x) for x in sorted([
+                board[k][l]
+                for l in list(range(j * 3, (j * 3) + 3))
+                for k in list(range(i - 1, i + 2))
+            ])
+        ]) != '123456789':
+            return False
+    
+    return True
+
 def valid_solution(board):
     for i in list(range(1, 10)):
         if ''.join([str(x) for x in sorted(board[i - 1])]) != '123456789':
@@ -10,15 +23,7 @@ def valid_solution(board):
         ]) != '123456789':
             return False
 
-        if i % 3 == 1:
-            for j in list(range(3)):
-                if ''.join([
-                    str(x) for x in sorted([
-                        board[k][l]
-                        for l in list(range(j * 3, (j * 3) + 3))
-                        for k in list(range(i - 1, i + 2))
-                    ])
-                ]) != '123456789':
-                    return False
+        if i % 3 == 1 and check_sub_squares(board, i) == False:
+            return False
 
     return True

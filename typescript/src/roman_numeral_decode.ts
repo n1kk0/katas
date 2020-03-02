3dea71a1f302d75c1a0e904907b44c926da942fd
n1kk0 <nicolas.moussikian@gmail.com>
2020-03-02 09:49:36 +0100

    maintainability

--- typescript/src/roman_numeral_decode.ts
+++ typescript/src/roman_numeral_decode.ts
@@ -1,31 +1,43 @@
+let twoLettersValues = new Map<string, number>();
+let oneLetterValues = new Map<string, number>();
+
+twoLettersValues.set("IV", 4);
+twoLettersValues.set("IX", 9);
+twoLettersValues.set("XL", 40);
+twoLettersValues.set("XC", 90);
+twoLettersValues.set("CD", 400);
+twoLettersValues.set("CX", 900);
+oneLetterValues.set("I", 1);
+oneLetterValues.set("V", 5);
+oneLetterValues.set("X", 10);
+oneLetterValues.set("L", 50);
+oneLetterValues.set("C", 100);
+oneLetterValues.set("D", 500);
+oneLetterValues.set("M", 1000);
+
 export function romanNumeralDecode(roman: string): number {
-    let twoLettersValues = new Map<string, number>();
-    let oneLetterValues = new Map<string, number>();
     let value = 0;
 
-    twoLettersValues.set("IV", 4);
-    twoLettersValues.set("IX", 9);
-    twoLettersValues.set("XL", 40);
-    twoLettersValues.set("XC", 90);
-    twoLettersValues.set("CD", 400);
-    twoLettersValues.set("CX", 900);
-    oneLetterValues.set("I", 1);
-    oneLetterValues.set("V", 5);
-    oneLetterValues.set("X", 10);
-    oneLetterValues.set("L", 50);
-    oneLetterValues.set("C", 100);
-    oneLetterValues.set("D", 500);
-    oneLetterValues.set("M", 1000);
-
     while (roman.length > 0) {
-        if (twoLettersValues.has(roman.substr(0, 2))) {
-            value += twoLettersValues.get(roman.substr(0, 2)) || 0;
-            roman = roman.substr(2);
-        } else {
-            value += oneLetterValues.get(roman.substr(0, 1)) || 0;
-            roman = roman.substr(1);
-        }
+        value += getNextValue(roman);
+        roman = getRemainingCharacters(roman);
     }
 
     return value;
+}
+
+function getNextValue(roman: string) {
+    if (twoLettersValues.has(roman.substr(0, 2))) {
+        return twoLettersValues.get(roman.substr(0, 2)) || 0;
+    } else {
+        return oneLetterValues.get(roman.substr(0, 1)) || 0;
+    }
+}
+
+function getRemainingCharacters(roman: string) {
+    if (twoLettersValues.has(roman.substr(0, 2))) {
+        return roman.substr(2);
+    } else {
+        return roman.substr(1);
+    }
 }

fce7610ad97221a6a01ba209b39d0c76370e6697
n1kk0 <nicolas.moussikian@gmail.com>
2020-03-02 02:27:47 +0100

    Avoid too many return statements

--- python/src/who_likes_it.py
+++ python/src/who_likes_it.py
@@ -1,15 +1,19 @@
 def who_likes_it(names):
+    message = ""
+
     if len(names) == 0:
-        return "no one likes this"
+        message = "no one likes this"
     elif len(names) == 1:
-        return "%s likes this" % names[0]
+        message = "%s likes this" % names[0]
     elif len(names) == 2:
-        return "%s and %s like this" % (names[0], names[1])
+        message = "%s and %s like this" % (names[0], names[1])
     elif len(names) == 3:
-        return "%s, %s and %s like this" % (names[0], names[1], names[2])
+        message = "%s, %s and %s like this" % (names[0], names[1], names[2])
     else:
-        return "%s, %s and %s others like this" % (
+        message = "%s, %s and %s others like this" % (
             names[0],
             names[1],
             len(names) - 2
         )
+
+    return message

7b1a335e924a9aafca51b57cc612b25819184966
n1kk0 <nicolas.moussikian@gmail.com>
2020-03-10 15:41:56 +0100

    pagination helper

---
+++ python/src/pagination_helper.py
@@ -0,0 +1,27 @@
+import math
+
+class PaginationHelper:
+    def __init__(self, collection, items_per_page):
+        self.collection = collection
+        self.items_per_page = items_per_page
+
+    def item_count(self):
+        return len(self.collection)
+
+    def page_count(self):
+        return math.ceil(len(self.collection) / self.items_per_page)
+
+    def page_item_count(self, page_index):
+        if page_index + 1 > self.page_count() or page_index < 0:
+            return -1
+
+        if page_index + 1 == self.page_count():
+            return len(self.collection) % self.items_per_page
+        
+        return self.items_per_page
+
+    def page_index(self, item_index):
+        if item_index > len(self.collection) - 1 or item_index < 0:
+            return -1
+
+        return math.floor(item_index / self.items_per_page)
---
+++ python/test/test_pagination_helper.py
@@ -0,0 +1,30 @@
+import unittest
+
+from src.pagination_helper import PaginationHelper
+
+class TestPaginationHelper(unittest.TestCase):
+    def test_paginaiton_helper(self):
+        collection = range(1,25)
+        helper = PaginationHelper(collection, 10)
+
+        self.assertEqual(
+            helper.page_count(),
+            3,
+            'page_count is returning incorrect value.'
+        )
+
+        self.assertEqual(
+            helper.page_index(23),
+            2,
+            'page_index returned incorrect value'
+        )
+
+        self.assertEqual(
+            helper.item_count(),
+            24,
+            'item_count returned incorrect value'
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
